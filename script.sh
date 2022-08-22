bcf4db73bd1b487a067153725295fbd6909efdb8
Arman Hr Shaikh <105963492+armanhrshaikh@users.noreply.github.com>
2022-08-22 15:17:55 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 pkg update
 pkg upgrade
-chmod 777 *
+chmod 755 *
 pkg install termux-api
 pkg install xdg-open
 pkg install tesseract
@@ -8,7 +8,6 @@ pkg install vim
 pkf install sed
 pkg install python
 pkg install shc
-chmod 777 *
+chmod 755 *
 shc -f sms.sh -o sms
 mv sms /data/data/com.termux/files/usr/bin/
-
