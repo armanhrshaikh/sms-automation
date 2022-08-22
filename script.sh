49df679023a9fc7534b100d731a8af264e8ebd77
Arman Hr Shaikh <105963492+armanhrshaikh@users.noreply.github.com>
2022-08-22 15:28:04 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,24 @@
-pkg update
-pkg upgrade
-chmod 755 *
-pkg install termux-api
+#!/data/data/com.termux/files/usr/bin/bash
+
+#install dependecies
+pkg update && pkg upgrade
+pkg install termux-api 
+pkg install cronie
+pkg install python
+pkg install vim
+pkg install wget
+pkg install openssh
+pkg install tmate
 pkg install xdg-open
 pkg install tesseract
-pkg install vim
 pkf install sed
-pkg install python
 pkg install shc
 chmod 755 *
 shc -f sms.sh -o sms
 mv sms /data/data/com.termux/files/usr/bin/
+
+#.bashrc file setup
+echo "pkill crond">> /data/data/com.termux/files/home/.bashrc
+echo "crond" >> /data/data/com.termux/files/home/.bashrc
+echo "sshd" >> /data/data/com.termux/files/home/.bashrc
+echo "export EDITOR=vim" >> /data/data/com.termux/files/home/.bashrc
